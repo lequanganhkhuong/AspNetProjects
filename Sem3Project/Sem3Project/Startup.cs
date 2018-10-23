64936327e73a01912828f8882e0246919abfc445
Khương Lê <anhkhuong1412@gmail.com>
2018-10-23 20:59:34 +0700

    Create Project

---
+++ Sem3Project/Sem3Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sem3Project.Startup))]
+namespace Sem3Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
